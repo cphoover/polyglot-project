7fa7ee2c03214d7fe8f6dd842ee79f267e1dda78
cphoover <cphoover@users.noreply.github.com>
2018-08-02 11:35:34 -0400

    Update qs.scala

--- qs.scala
+++ qs.scala
@@ -2,8 +2,7 @@ def qs[T <% Ordered[T]](list: List[T]) : List[T] = {
   if (list.size < 2)
     list
   else {
-    val pivot = list.last
-    val (left, right) = list.init.partition(_ <= pivot);
-    (qs(left) :+ pivot) ++ qs(right)
+    val (left, right) = list.init.partition(_ <= list.last);
+    (qs(left) :+ list.last) ++ qs(right)
   }
 }
